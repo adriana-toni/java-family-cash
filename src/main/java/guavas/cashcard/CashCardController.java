5080850ad5282ba883ded2c7bbe4b0515de73d0a
adriana.toni <adriana.toni@gmail.com>
2024-08-14 18:08:45 -0300

    Paging and Sorting defaults

--- src/main/java/guavas/cashcard/CashCardController.java
+++ src/main/java/guavas/cashcard/CashCardController.java
@@ -4,6 +4,7 @@ import guavas.cashcard.repositories.CashCardRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,7 +29,7 @@ public class CashCardController {
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSort()
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
                 ));
         return ResponseEntity.ok(page.getContent());
     }
