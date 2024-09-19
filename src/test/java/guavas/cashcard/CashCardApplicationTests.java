4a5a3c2f5bc3ed9c11c90301da3dae0d431affff
adriana.toni <adriana.toni@gmail.com>
2024-09-19 07:47:14 -0300

    TDD approach: Attempt to delete a Cash Card that does not exist

--- src/test/java/guavas/cashcard/CashCardApplicationTests.java
+++ src/test/java/guavas/cashcard/CashCardApplicationTests.java
@@ -220,4 +220,12 @@ class CashCardApplicationTests {
 				.getForEntity("/cashcards/99", String.class);
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
+
+	@Test
+	void shouldNotDeleteACashCardThatDoesNotExist() {
+		ResponseEntity<Void> deleteResponse = restTemplate
+				.withBasicAuth("Adriana", "abc123")
+				.exchange("/cashcards/99999", HttpMethod.DELETE, null, Void.class);
+		assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
 }
